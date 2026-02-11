5c86c8fee1f1b43559ac479e3e2c46c65e13867f
23146ps <23146ps@hvhs.school.nz>
2026-02-11 15:15:30 +1300

    11/02 end

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -13,11 +13,22 @@ function setup() {
 
 	world.gravity.y = 10;
 
-	sprite = new Sprite(500,100,200,200);
+	sprite = new Sprite(width/2,height/2,20);
 	sprite.color = 'blue'
-	sprite.rotationSpeed = 2;
+	sprite.bounciness = 1;
 	sprite.vel.x = 2;
-	platform_1 = new Sprite(500,300,1000,20,'k')
+	sprite.friction = 0;
+	sprite.drag = 0;
+	//platform_1 = new Sprite(500,300,1000,20,'k')
+
+	wallLH  = new Sprite(0, height/2, 8, height, 'k');
+	wallLH.color = 'black';
+	wallRH  = new Sprite(width, height/2, 8, height, 'k');
+	wallRH.color = 'black'
+	wallTop = new Sprite(width/2, 0, width, 8, 'k');
+	wallTop.color = 'black'
+	wallBot = new Sprite(width/2, height, width, 8, 'k');
+	wallBot.color = 'black'
 }
 	
 /*******************************************************/
@@ -25,6 +36,12 @@ function setup() {
 /*******************************************************/
 function draw() {
 	background('ccc');
+
+	sprite = new Sprite(width/2,height/2,10);
+	sprite.color = 'blue'
+	sprite.bounciness = 0.7;
+	sprite.friction = 0;
+	//sprite.drag = 0;
 }
 
 /*******************************************************/
--- t06_createAliens.js
+++ t06_createAliens.js
@@ -9,14 +9,34 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+	cnv = new Canvas(windowWidth, windowHeight);
 
+	world.gravity.y = 10;
+
+	sprite = new Sprite(width/2,height/2,20);
+	sprite.color = 'blue'
+	sprite.bounciness = 1;
+	sprite.vel.x = 2;
+	sprite.friction = 0;
+	sprite.drag = 0;
+	//platform_1 = new Sprite(500,300,1000,20,'k')
+
+	wallLH  = new Sprite(0, height/2, 8, height, 'k');
+	wallLH.color = 'black';
+	wallRH  = new Sprite(width, height/2, 8, height, 'k');
+	wallRH.color = 'black'
+	wallTop = new Sprite(width/2, 0, width, 8, 'k');
+	wallTop.color = 'black'
+	wallBot = new Sprite(width/2, height, width, 8, 'k');
+	wallBot.color = 'black'
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('ccc');
+
 }
 
 /*******************************************************/
